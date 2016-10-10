047070d6b3608cf682e7f672d854bcf305b803e9
Raquel Perez Dominguez <raquelpd@gamma39.aulas.gsyc.urjc.es>
2016-10-10 15:12:35 +0200

    listado ordenado de etiquetas.Pep8

--- karaoke.py
+++ karaoke.py
@@ -6,42 +6,29 @@ from xml.sax.handler import ContentHandler
 import smallsmilhandler
 import sys
 
-#def __init__(Self, fichero):
-#    
-#    parser = make_parser()
-#    cHandler = smallsmilhandler.SmallSMILHandler()
-#    parser.setContentHandler(cHandler)
-#    parser.parse(fich)
-#    datos = cHandler.get_tags()
 
 def elems(datos):
-    elem =''
+    elem = ''
     for lista in datos:
         elem = lista[0]
         sublista = lista[1]
         for atributo in sublista:
-            elem = elem + "\t" + atributo + "=" + sublista[atributo] + " " 
-            
+            elem = elem + "\t" + atributo + "=" + sublista[atributo] + " "
         print(elem + "\n")
-            
-        
+
 
 if __name__ == "__main__":
-       
+
     try:
         fichero = sys.argv[1]
     except IndexError:
-         sys.exit("Usage: python3 karaoke.py file.smil")
+        sys.exit("Usage: python3 karaoke.py file.smil")
 
-    parser = make_parser()  
-    cHandler = smallsmilhandler.SmallSMILHandler() 
-    parser.setContentHandler(cHandler)    
+    parser = make_parser()
+    cHandler = smallsmilhandler.SmallSMILHandler()
+    parser.setContentHandler(cHandler)
     fich = open(sys.argv[1], 'r')
     parser.parse(fich)
     datos = cHandler.get_tags()
-        
-    elems(datos)
-     
 
-   
-   
+    elems(datos)
