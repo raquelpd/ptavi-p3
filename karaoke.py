1f1d688d9039605c8bdfda8be23504793e5806cf
Raquel Perez Dominguez <raquelpd@gamma39.aulas.gsyc.urjc.es>
2016-10-10 17:59:12 +0200

    apartado 6

--- karaoke.py
+++ karaoke.py
@@ -3,51 +3,65 @@
 
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
+from smallsmilhandler import SmallSMILHandler
 import smallsmilhandler
 import sys
 import json
 from urllib.request import urlretrieve
 
 
-def print_elementos(datos):
-    elem = ''
-    for lista in datos:
-        elem = lista[0]
-        sublista = lista[1]
-        for atributo in sublista:
-            elem = elem + "\t" + atributo + "=" + sublista[atributo] + " "
-        print(elem + "\n")
+class KaraokeLocal(SmallSMILHandler):
 
+    def __init__(self,fich):
+        parser = make_parser()
+        cHandler = smallsmilhandler.SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        fich = open(sys.argv[1], 'r')
+        parser.parse(fich)
+        self.datos = cHandler.get_tags()
 
-def modifica_url(datos):
-    for lista in datos:
-        sublista = lista[1]
-        for atributo in sublista:
-            if sublista[atributo][:7] == "http://":
-                urlretrieve(sublista[atributo])
-                print(sublista[atributo])
-                url = sublista[atributo].split('/')
-                sublista[atributo] = url[-1]
-                print(sublista[atributo])
+    def __str__(self):
+        elem = ''
+        for lista in self.datos:
+            elem = lista[0]
+            sublista = lista[1]
+            for atributo in sublista:
+                elem = elem + "\t" + atributo + "=" + sublista[atributo] + " "
+            print(elem + "\n")
+        return(elem)
+
+    def to_json(self, fich, new_fich = ""):
+        if new_fich == "":
+            nf = fich[:fich.find('.')]
+        else:
+            nf = new_fich
+        fich_json = open(nf + '.json', 'w')
+        json.dump(self.datos, fich_json, sort_keys=True, indent=4, separators=(',', ':'))
+        fich_json.close()
+     
+
+    def do_local(self):
+        for lista in self.datos:
+            sublista = lista[1]
+            for atributo in sublista:
+                if sublista[atributo][:7] == "http://":
+                    urlretrieve(sublista[atributo])
+                    print(sublista[atributo])
+                    url = sublista[atributo].split('/')
+                    sublista[atributo] = url[-1]
+                    print(sublista[atributo])
 
 if __name__ == "__main__":
 
     try:
-        fichero = sys.argv[1]
+        fich = sys.argv[1]
+        karaoke = KaraokeLocal(fich)
+        print(karaoke)
+        karaoke.do_local()
+        karaoke.to_json(fich)
+        print(karaoke)
+
     except IndexError:
         sys.exit("Usage: python3 karaoke.py file.smil")
+    
 
-    parser = make_parser()
-    cHandler = smallsmilhandler.SmallSMILHandler()
-    parser.setContentHandler(cHandler)
-    fich = open(sys.argv[1], 'r')
-    parser.parse(fich)
-    datos = cHandler.get_tags()
-
-    print_elementos(datos)
-
-    fich_json = open('karaoke.json', 'w')
-    json.dump(datos, fich_json, sort_keys=True, indent=4, separators=(',', ':'))
-    fich_json.close()
-
-    modifica_url(datos)
