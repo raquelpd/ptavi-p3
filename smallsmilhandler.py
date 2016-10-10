0defeb1881b30a0791c44a050793b4252e624f29
Raquel Perez Dominguez <raquelpd@gamma38.aulas.gsyc.urjc.es>
2016-10-10 09:49:17 +0200

    Apartado 4.Pep8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,41 +4,37 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
 
-	def __init__ (self):
-       
-		self.etiquetas = []
-		
-		self.atributos = {'root-layout': ['width', 'height', 'background-color'],
-            'region': ['id', 'top', 'bottom', 'left', 'right'],
-            'img': ['src', 'region', 'begin', 'dur'],
-            'audio': ['src', 'begin', 'dur'],
-           'textstream': ['src', 'region']}
+    def __init__(self):
 
-		
-	def startElement(self, name, attrs):
+        self.etiquetas = []
 
-		if name in self.atributos:
-			dicc = {}
-			for atributo in self.atributos[name]:
-				dicc[atributo] = attrs.get(atributo, "")
-			self.etiquetas.append([name, dicc])
-	
-	def get_tags(self):
-		
-		return self.etiquetas
+        self.atributos = {'root-layout': ['width', 'height', 'background-color'],
+                          'region': ['id', 'top', 'bottom', 'left', 'right'],
+                          'img': ['src', 'region', 'begin', 'dur'],
+                          'audio': ['src', 'begin', 'dur'],
+                          'textstream': ['src', 'region']}
+
+    def startElement(self, name, attrs):
+
+        if name in self.atributos:
+            dicc = {}
+            for atributo in self.atributos[name]:
+                dicc[atributo] = attrs.get(atributo, "")
+            self.etiquetas.append([name, dicc])
+
+    def get_tags(self):
+
+        return self.etiquetas
 
 
 if __name__ == "__main__":
-	
-	
-	parser = make_parser()
-	cHandler = SmallSMILHandler()
-	parser.setContentHandler(cHandler)
-	parser.parse(open('karaoke.smil'))
-	datos = cHandler.get_tags()
-	print(datos)
 
-	
-
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open('karaoke.smil'))
+    datos = cHandler.get_tags()
+    print(datos)
