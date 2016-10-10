58b7e592f4a7fc661b621be46c53231f37d213d7
Raquel Perez Dominguez <raquelpd@gamma38.aulas.gsyc.urjc.es>
2016-10-10 09:27:34 +0200

    Apartado 4.Clase contenthandler Sin pep8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,4 +1,43 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+class SmallSMILHandler(ContentHandler):
+
+	def __init__ (self):
+       
+		self.etiquetas = []
+		
+		self.atributos = {'root-layout': ['width', 'height', 'background-color'],
+            'region': ['id', 'top', 'bottom', 'left', 'right'],
+            'img': ['src', 'region', 'begin', 'dur'],
+            'audio': ['src', 'begin', 'dur'],
+           'textstream': ['src', 'region']}
+
+		
+	def startElement(self, name, attrs):
+
+		if name in self.atributos:
+			dicc = {}
+			for atributo in self.atibutos[name]:
+				dicc[atributo] = attrs.get(atributo, "")
+			self.etiquetas.append([name, dicc])
+	
+		def get_tags(self):
+			return self.etiquetas
+
+
+if __name__ == "__main__":
+	
+	
+	parser = make_parser()
+	cHandler = SmallSMILHandler()
+	parser.setContentHandler(cHandler)
+	parser.parse(open('karaoe.smil))
+	datos = cHandler.get_tags()
+	print(datos)
+
+	
 
